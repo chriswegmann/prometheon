11265028d7a8531a7029cdc9b7324ec83fcf4c8c
Ernst <ernst.oldenhof@gmail.com>
2018-03-07 08:29:59 +0100

    added data analysis files and notebook in /analytics

--- code/models/user.py
+++ code/models/user.py
@@ -1,11 +1,4 @@
-
 import sqlite3
-
-# NB: class User is not a resource, but a helper. The API does not deal with
-# this class directly
-# A model is an internal representation of an object
-# A resource is what the API interacts with
-
 
 class UserModel(object):
     def __init__(self, id, username, password):
